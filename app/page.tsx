6583969cc8ffe2623f6f24d13e59baf651147671
HasinaSp <hasina.and@prestigg.io>
2026-01-07 14:48:42 +0300

    feature: update for Assessment & Resources

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import CircularMaturity from "@/components/sections/CircularMaturity";
 import Features from "@/components/sections/Features";
+import FreeAssessment from "@/components/sections/FreeAssessment";
 import Hero from "@/components/sections/Hero";
+import Resources from "@/components/sections/Resources";
 import WhyCircular from "@/components/sections/WhyCircular";
 
 export default function HomePage() {
@@ -10,6 +12,8 @@ export default function HomePage() {
       <WhyCircular />
       <Features />
       <CircularMaturity/>
+      <Resources />
+      <FreeAssessment />
     </main>
   );
 }
