0637c67f8c57eccb927054d03c3c2efa8570d31d
Landry Rakotoarivelo <lrakoto@danapay.io>
2026-01-09 16:08:51 +0300

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Header from "@/components/layout/Header";
 
 export default function RootLayout({
   children,
@@ -9,7 +8,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased text-slate-900">
-        <Header />
         {children}
       </body>
     </html>
