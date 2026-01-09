c0dea80dd7c43769e7f61726d3f25c038405b13d
Landry Rakotoarivelo <lrakoto@danapay.io>
2026-01-09 19:03:02 +0300

    ok

--- components/assessement/AssessementCTA.tsx
+++ components/assessement/AssessementCTA.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function AssessmentCTA() {
   return (
     <Link
-      href="/assessment/start"
+      href="/assessment/basic"
       className="inline-flex items-center justify-center rounded-full bg-white px-6 py-3 text-sm font-semibold text-blue-900 transition hover:bg-blue-50"
     >
       Start My Assessment
