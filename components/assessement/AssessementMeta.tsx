c7ff0c7529c021deaff0407eab9be6193cfbb790
HasinaSp <hasina.and@prestigg.io>
2026-01-09 17:14:23 +0300

    feature: update for assessment and auth pages

--- components/assessement/AssessementMeta.tsx
+++ components/assessement/AssessementMeta.tsx
@@ -4,12 +4,12 @@ export default function AssessmentMeta() {
   return (
     <div className="mb-8 flex flex-wrap items-center gap-6 text-sm text-white/80">
       <span className="flex items-center gap-2">
-        <Image src="/icons/clock.svg" alt="" width={16} height={16} />
+        <Image src="/diagnostic/clock -blanc.png" alt="" width={16} height={16} />
         Estimated duration: ~15 minutes
       </span>
 
       <span className="flex items-center gap-2">
-        <Image src="/icons/help.svg" alt="" width={16} height={16} />
+        <Image src="/diagnostic/question.png" alt="" width={16} height={16} />
         33 questions
       </span>
     </div>
